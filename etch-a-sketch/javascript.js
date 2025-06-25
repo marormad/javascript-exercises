ab3dc0199c980cbfce4cf47e628d4fe70ce14d0d
marormad <marcortizm2@hotmail.com>
2025-06-25 21:57:44 +0200

    Basic grid with fixed size and just one color

--- etch-a-sketch/javascript.js
+++ etch-a-sketch/javascript.js
@@ -1,7 +1,23 @@
+// The div that is already created in the html and will contain all the squares that will be colored
 const container = document.getElementById('bigContainer');
 
+// Creation of 16 divs, that will appear as a grid with CSS
+// Basic grid with fixed size and just one color
 for (let i = 0; i < 16; i++) {
     let divi = document.createElement('div');
     divi.setAttribute('id', `divi_${i}`);
+    divi.setAttribute('class', 'divi');
     container.append(divi);
 }
+
+// On mouse hover, background color of a 'squared div' should change
+const divis = document.querySelectorAll('.divi');
+
+divis.forEach((divi) => {
+    divi.addEventListener('mouseenter', function () {
+        this.style.backgroundColor = "red";
+    });
+});
+
+
+
